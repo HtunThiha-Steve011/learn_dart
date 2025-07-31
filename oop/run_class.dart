5483e836f677cb1582118fe8eb4860a394e30fd6
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-07-31 23:07:00 +0800

    Add class, abstract class and inheritance demo

---
+++ oop/run_class.dart
@@ -0,0 +1,19 @@
+import 'class.dart';
+
+void main() {
+  Person person1 = Person('Steve', 20, 'M');
+  Person person2 = Person.female('Sally', 19);
+
+  print('===== Person class demo =====');
+  person1.greet();
+  person2.greet();
+  print('');
+
+  // Abstract class cannot be initialized; this will give compile-time error
+  // LivingEntity entity = LivingEntity(1, 19, 4.5);
+
+  print('===== LivingEntity and Dog class demo');
+  Dog dog1 = Dog.mediumHungerTolerance(1, 2, 'Milo', 10.0);
+  dog1.describeLivingEntity();
+  dog1.eat();
+}
